3e0f6f255d7bf50b4ebaf345ce60b3ea1f5b57d8
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-12-08 23:37:16 +0600

    added react helmet npm

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./layout/Main.jsx";
 import Home from "./pages/Home/Home.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>
 );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,41 +1,8 @@
 import React from "react";
 import Container from "../../components/common/Container";
-import Heading from "../../components/common/Heading";
-import Button from "../../components/common/Button";
 
 const Home = () => {
-  return (
-    <Container>
-      <Heading title="home" />
-      <Button title={"Get Started"} isPrimary={true} />
-      <span className="font-noto">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima dolores
-        nihil porro eius similique suscipit laudantium. Quia reiciendis
-        cupiditate facere, omnis expedita quod, velit minus maxime magnam,
-        exercitationem repudiandae voluptatibus aspernatur voluptatum deserunt
-        reprehenderit obcaecati nostrum nobis ut amet ipsum architecto nemo sunt
-        perferendis a! Voluptas earum atque perspiciatis veniam aut reiciendis
-        fuga pariatur. Et expedita provident veritatis? Corporis dicta debitis
-        earum omnis nihil molestias eveniet accusantium! Atque quidem laboriosam
-        beatae in cupiditate provident iure dicta enim necessitatibus, dolorem
-        rerum et modi culpa voluptatum illum totam voluptatem similique
-        voluptatibus tempora. Nisi obcaecati ducimus exercitationem! Velit
-        accusantium, impedit rerum veniam fuga tempora magni, suscipit
-        reiciendis dolor, deserunt doloribus ut quidem sequi. Cupiditate
-        excepturi cum illum quasi facere ut inventore officiis quos, ad
-        voluptatem tenetur, laborum quidem quisquam quod veniam architecto! Ab
-        unde totam incidunt accusantium error aperiam quae corporis, facere
-        laudantium magnam modi, voluptates dicta. Quae quam perferendis ad
-        aperiam perspiciatis molestias sed, nostrum nobis ex quibusdam rerum qui
-        facilis reiciendis eveniet excepturi odio inventore quisquam libero
-        necessitatibus corporis obcaecati eius. Dolor dolorum dolores at magni
-        maxime sapiente sit, illo, quasi doloremque placeat atque quibusdam sunt
-        nesciunt itaque tempore voluptas adipisci impedit aperiam exercitationem
-        corrupti error consequatur pariatur? Earum, quia esse!
-        <h1>This is title</h1>
-      </span>
-    </Container>
-  );
+  return <Container></Container>;
 };
 
 export default Home;
