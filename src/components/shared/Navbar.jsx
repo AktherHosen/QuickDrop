747c4053617f150dcaa0517738560e2d50cdaea5
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-12-13 23:30:02 +0600

    update navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,155 +1,82 @@
-import React from "react";
-import { IoMdNotifications } from "react-icons/io";
+import React, { useState } from "react";
 import Logo from "../../assets/images/logo.png";
+import { FaHome } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
+import Container from "../common/Container";
+import { IoNotificationsSharp } from "react-icons/io5";
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="bg-white border dark:bg-neutral-800 dark:border-neutral-700 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full">
-      <nav className="relative max-w-[1240px] min-h-[50px] px-6 sm:px-4 lg:px-2  w-full md:flex md:items-center md:justify-between md:gap-3 mx-auto py-2">
-        <div className="flex items-center justify-between">
-          <a
-            className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80 dark:text-white flex items-center gap-1"
-            href="#"
-            aria-label="Brand"
-          >
-            <img src={Logo} className="h-[50px]" alt="" />
-            <span className="hidden lg:block text-[17px]">Quick Drop</span>
-          </a>
-
-          <div className="md:hidden">
-            <button
-              type="button"
-              className="hs-collapse-toggle relative size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-              id="hs-header-classic-collapse"
-              aria-expanded="false"
-              aria-controls="hs-header-classic"
-              aria-label="Toggle navigation"
-              data-hs-collapse="#hs-header-classic"
-            >
-              <svg
-                className="hs-collapse-open:hidden size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <line x1="3" x2="21" y1="6" y2="6" />
-                <line x1="3" x2="21" y1="12" y2="12" />
-                <line x1="3" x2="21" y1="18" y2="18" />
-              </svg>
-              <svg
-                className="hs-collapse-open:block shrink-0 hidden size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
-              <span className="sr-only">Toggle navigation</span>
-            </button>
-          </div>
+    <Container>
+      <nav className="flex justify-between items-center relative">
+        <div className="flex items-center gap-1">
+          <img src={Logo} alt="Logo" className="h-14 w-14" />
+          <h1 className="font-poppins items-center text-lg hidden lg:flex font-semibold">
+            Quick Drop
+          </h1>
         </div>
 
-        <div
-          id="hs-header-classic"
-          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
-          aria-labelledby="hs-header-classic-collapse"
+        <ul className="hidden font-raleway lg:flex items-center gap-x-2 text-sm ">
+          <li className="py-2 hover:underline hover:underline-offset-4 transition-all duration-300">
+            <a href="#">Home</a>
+          </li>
+          <li className="py-2 hover:underline hover:underline-offset-4 transition-all duration-300">
+            <a href="#">Dashboard</a>
+          </li>
+          <li className="py-2 hover:underline hover:underline-offset-4 transition-all duration-300 flex items-center">
+            <IoNotificationsSharp className="text-xl hover:text-[#75a3e9] transition-all duration-300" />
+            <a href="#" className="lg:hidden">
+              Notification
+            </a>
+          </li>
+        </ul>
+        <span
+          className="lg:hidden cursor-pointer transition-all duration-400"
+          onClick={toggleMenu}
         >
-          <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-            <div className="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
-              <a
-                className="p-2 flex items-center text-sm text-blue-600 focus:outline-none focus:text-blue-600 dark:text-blue-500 dark:focus:text-blue-500"
-                href="#"
-                aria-current="page"
-              >
-                <svg
-                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                  <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                </svg>
-                Home
-              </a>
+          {isMenuOpen ? (
+            <IoCloseSharp size={30} />
+          ) : (
+            <GiHamburgerMenu size={30} />
+          )}
+        </span>
 
-              <a
-                className="p-2 flex items-center text-sm text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
-                href="#"
-              >
-                <svg
-                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                  <circle cx="12" cy="7" r="4" />
-                </svg>
-                Dashboard
-              </a>
+        <ul
+          className={`flex flex-col text-start ps-4 rounded-md absolute top-20 z-10 left-0 w-full bg-primary bg-opacity-20 text-black  lg:hidden 
+          transition-all duration-300 transform origin-top ${
+            isMenuOpen
+              ? "scale-y-100 transition-all duration-400"
+              : "scale-y-0 transition-all duration-400"
+          }`}
+        >
+          <li className="py-2 hover:underline hover:underline-offset-4 transition-all duration-300 flex gap-1 items-center">
+            <FaHome className="text-[14px] lg:hidden hover:text-[#75a3e9] transition-all duration-300" />
+            <a href="#">Home</a>
+          </li>
+          <li className="py-2 hover:underline hover:underline-offset-4 transition-all duration-300 flex gap-1 items-center">
+            <TbLayoutDashboardFilled className="text-[14px] lg:hidden hover:text-[#75a3e9] transition-all duration-300" />
+            <a href="#" className="text-[15px]">
+              Dashboard
+            </a>
+          </li>
 
-              <a
-                className="p-2 flex items-center text-sm text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
-                href="#"
-              >
-                <IoMdNotifications className="shrink-0 size-4 me-3 md:me-2 " />
-
-                <span className="block md:hidden"> Notification</span>
-              </a>
-
-              <div className="relative flex flex-wrap items-center gap-x-1.5 md:ps-2.5 mt-1 md:mt-0 md:ms-1.5 before:block before:absolute before:top-1/2 before:-start-px before:w-px before:h-4 before:bg-gray-300 before:-translate-y-1/2 dark:before:bg-neutral-700">
-                <a
-                  className="p-2 w-full flex items-center text-sm text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
-                  href="#"
-                >
-                  <svg
-                    className="shrink-0 size-4 me-3 md:me-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                    <circle cx="12" cy="7" r="4" />
-                  </svg>
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+          <li className="py-2 hover:underline hover:underline-offset-4 transition-all duration-300 flex gap-1 items-center">
+            <IoNotificationsSharp className="text-[14px] lg:text-xl hover:text-[#75a3e9] transition-all duration-300" />
+            <a href="#" className="lg:hidden text-[15px]">
+              Notification
+            </a>
+          </li>
+        </ul>
       </nav>
-    </header>
+    </Container>
   );
 };
 
