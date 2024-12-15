8cd99dbaecaf9d67855ddb7ef4c0dd952bd712cf
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-12-15 07:56:48 +0600

    update logo

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ title, isPrimary, isSmall }) => {
   return (
     <button
-      className={`px-4 py-1.5 rounded-sm cursor-pointer block
+      className={`px-4 py-1.5 rounded-sm cursor-pointer  block
     
       ${
         isPrimary
