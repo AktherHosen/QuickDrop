6f63d2e08e11310b53d2d8a2cf4f832f8cae93f2
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-12-17 20:21:42 +0600

    update

--- src/components/home/DeliveryThings.jsx
+++ src/components/home/DeliveryThings.jsx
@@ -11,14 +11,14 @@ const DeliveryThings = () => {
         <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
           Delivering Everything You Need
         </h1>
-        <h4 className="text-sm">
+        <h4 className="text-xs md:text-sm">
           Fast, reliable, and hassle-free delivery of groceries, packages,
           meals, and more!
         </h4>
       </div>
-      <div className="h-[200px] grid grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-8 lg:gap-4 mt-8">
-        <div className="min-h[100px] border shadow-sm transition-all duration-300 rounded-md hover:border-primary px-6 py-2 flex justify-center flex-col items-center gap-4">
-          <BsFillBoxSeamFill className="text-[40px] md:text-[50px] lg:text-[60px] hover:text-primary transition-colors duration-300" />
+      <div className="min-h-[200px] grid grid-cols-2 lg:grid-cols-4 items-center gap-x-4 gap-y-8 lg:gap-4 mt-8">
+        <div className="min-h-[100px] border border-gray-100 shadow-sm transition-all duration-300 rounded-md border-b-[3.2px] hover:ease-linear hover:border-b-primary px-6 py-2.5 flex justify-center flex-col items-center gap-4">
+          <BsFillBoxSeamFill className="text-[40px] md:text-[50px] hover:text-primary transition-colors duration-300" />
           <div className="space-y-2 text-center">
             <span className="block">Package</span>
             <button className="bg-primary text-white px-3 py-1.5 rounded-sm text-xs">
@@ -26,29 +26,29 @@ const DeliveryThings = () => {
             </button>
           </div>
         </div>
-        <div className="min-h[100px] border shadow-sm transition-all duration-300 rounded-md hover:border-primary px-6 py-2 flex justify-center flex-col items-center gap-4">
-          <BsGiftFill className="text-[40px] md:text-[50px] lg:text-[60px] hover:text-primary transition-colors duration-300" />
+        <div className="min-h-[100px] border border-gray-100 shadow-sm transition-all duration-300 rounded-md border-b-[3.2px] hover:ease-linear hover:border-b-primary px-6  py-2.5 flex justify-center flex-col items-center gap-4">
+          <BsGiftFill className="text-[40px] md:text-[50px] hover:text-primary transition-colors duration-300" />
           <div className="space-y-2 text-center">
             <span className="block">Gift</span>
-            <button className="bg-primary text-white px-3 py-1.5 rounded-sm text-sm">
+            <button className="bg-primary text-white px-3 py-1.5 rounded-sm text-xs">
               Book Now
             </button>
           </div>
         </div>
-        <div className="min-h[100px] border shadow-sm transition-all duration-300 rounded-md hover:border-primary px-6 py-2 flex justify-center flex-col items-center gap-4">
-          <FaThList className="text-[40px] md:text-[50px] lg:text-[60px] hover:text-primary transition-colors duration-300" />
+        <div className="min-h-[100px] border border-gray-100 shadow-sm transition-all duration-300 rounded-md border-b-[3.2px] hover:ease-linear hover:border-b-primary px-6  py-2.5 flex justify-center flex-col items-center gap-4">
+          <FaThList className="text-[40px] md:text-[50px] hover:text-primary transition-colors duration-300" />
           <div className="space-y-2 text-center">
             <span className="block">Accessories</span>
-            <button className="bg-primary text-white px-3 py-1.5 rounded-sm text-sm">
+            <button className="bg-primary text-white px-3 py-1.5 rounded-sm text-xs">
               Book Now
             </button>
           </div>
         </div>
-        <div className="min-h[100px] border shadow-sm transition-all duration-300 rounded-md hover:border-primary px-6 py-2 flex justify-center flex-col items-center gap-4">
-          <HiDocumentText className="text-[40px] md:text-[50px] lg:text-[60px] hover:text-primary transition-colors duration-300" />
+        <div className="min-h-[100px] border border-gray-100 shadow-sm transition-all duration-300 rounded-md border-b-[3.2px] hover:ease-linear hover:border-b-primary px-6  py-2.5 flex justify-center flex-col items-center gap-4">
+          <HiDocumentText className="text-[40px] md:text-[50px]  hover:text-primary transition-colors duration-300" />
           <div className="space-y-2 text-center">
             <span className="block">Document</span>
-            <button className="bg-primary text-white px-3 py-1.5 rounded-sm text-sm">
+            <button className="bg-primary text-white px-3 py-1.5 rounded-sm text-xs">
               Book Now
             </button>
           </div>
