f8128eceefccb192e8122f9e35a653a742719d92
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-12-25 22:33:31 +0600

    update

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     setShowPassword(!showPassword);
   };
   return (
-    <section className="max-w-[1240px] h-screen flex flex-col justify-center  mx-auto px-6 sm:px-5 lg:px-4">
+    <section className="max-w-[1240px] h-screen  mx-auto px-6 sm:px-5 lg:px-4">
       <Link
         to="/"
         className="my-6 text-white font-semibold rounded-full bg-black flex items-center justify-center  p-2 w-fit"
